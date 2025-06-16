fd884a3f62e0004f67571faf2f828bfb88dd8955
Ronak1718 <ronakjangir590@gmail.com>
2025-06-16 18:53:24 +0530

    refined version

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let specialtisation = [{
-    image: 'eye_care.jpg',  title: 'Eye Care'
+    image: 'images/services/eye_care.jpg',  title: 'Eye Care'
 },
 {
-    image: 'cold.webp',  title: 'Cough and Cold'
+    image: 'images/services/cold.webp',  title: 'Cough and Cold'
 },
 {
-    image: ''
-},{},{},{},{},{}];
+    image: 'images/services/tension.webp' , title: 'Hypertension'
+},{
+    image: 'images/services/ORTHOPAEDICS-1.jpg' , title: 'Othropaedics'
+},{
+    image: 'images/services/children_doctor.jpg' , title: 'Peditrician'
+},{
+    image: 'images/services/heart.jpg' , title: 'cardiologist'
+}];
 let gridHTML = '';
 
 specialtisation.forEach((grid) => {
@@ -16,9 +22,28 @@ specialtisation.forEach((grid) => {
                 <div class="text_in_grid">
                 <br>
                 <h2>${grid.title}</h2>
+                <button class = "booking_buttons">Book Offline Appointment</button>
+                 <button class = "booking_buttons">Get Online Prescription</button>
                 </div>
                 </div>
             </div>`
 });
 
       document.querySelector('.available_categories').innerHTML = gridHTML;
+
+       document.addEventListener("DOMContentLoaded", () => {
+        const darkToggle = document.getElementById("darkModeSwitch");
+        const body = document.body;
+
+  const isDark = localStorage.getItem("darkMode") === "true";
+  if (isDark) body.classList.add("dark");
+  if (darkToggle) darkToggle.checked = isDark;
+
+  if (darkToggle) {
+    darkToggle.addEventListener("change", () => {
+      body.classList.toggle("dark");
+      localStorage.setItem("darkMode", body.classList.contains("dark"));
+    });
+  }
+});
+  
