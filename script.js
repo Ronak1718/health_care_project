4d2b60102725a61a0a4ecd47d58db070e0b9a198
Ronak1718 <ronakjangir590@gmail.com>
2025-06-16 12:20:56 +0530

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let specialtisation = []
+let specialtisation = [{
+    image: 'eye_care.jpg',  title: 'Eye Care'
+},
+{
+    image: 'cold.webp',  title: 'Cough and Cold'
+},
+{
+    image: ''
+},{},{},{},{},{}];
+let gridHTML = '';
+
+specialtisation.forEach((grid) => {
+    gridHTML += `<div class="item">
+                <div class="image_of_type">
+                <img src="${grid.image}" class = "service_image">
+                <div class="text_in_grid">
+                <br>
+                <h2>${grid.title}</h2>
+                </div>
+                </div>
+            </div>`
+});
+
+      document.querySelector('.available_categories').innerHTML = gridHTML;
